d25fe57bb4ba9f01a2f495361fe0944ed8dab2ea
caitanojunior <caitanojunior@gmail.com>
2017-03-15 19:18:10 -0300

    implementação do algoritmi ordenacao

--- Coursera/semana3/ordenacao.py
+++ Coursera/semana3/ordenacao.py
@@ -1,6 +1,15 @@
 numero = ((input('Digite um número inteiro: ')))
 
-num = len(numero)
 
-for i in range(num):
-    print(numero[i])
+primeiroNum = numero[0:1]
+segundoNum = numero[1:2]
+terceiroNum = numero[2:3]
+
+num1 = int(primeiroNum)
+num2 = int(segundoNum)
+num3 = int(terceiroNum)
+
+if(num1 < num2 < num3):
+    print('crescente')
+else:
+    print('decrescente')
