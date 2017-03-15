15b630f522237703e166d66846b1d8c7a6daa3ef
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-03-15 16:43:59 -0300

    exercicios programacao 3 semana

---
+++ Coursera/Fizz.py
@@ -0,0 +1,34 @@
+numero = int((input('Digite um número inteiro: ')))
+
+if(numero%3==0):
+    print('Fizz')
+else:
+    print(numero)
+
+
+'''
+Este código soma algarismos de um número digitado
+
+soma = 0
+
+for x in numero:
+    if x.isdigit():
+        soma += int(x)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Coursera/ordenacao.py
@@ -0,0 +1,6 @@
+numero = ((input('Digite um número inteiro: ')))
+
+num = len(numero)
+
+for i in range(num):
+    print(numero[i])
