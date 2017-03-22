95261e46ffced5a2dbd96f254c35b875a6aa1087
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-03-22 14:11:26 -0300

    correcao logica primalidade

--- Coursera/semana4/primalidade.py
+++ Coursera/semana4/primalidade.py
@@ -1,5 +1,21 @@
 numero = int(input('Digite um número inteiro: '))
 
+
+i = 2
+ehprimo = True
+
+while i < numero and ehprimo:
+	if numero % i == 0:
+		ehprimo = False
+	i = i + 1
+
+if(ehprimo):
+	print('primo')
+else:
+	print('não é primo')
+
+
+'''
 for i in range(2, numero+1):
 	if i != numero:
 		i = numero % i
@@ -10,3 +26,4 @@ for i in range(2, numero+1):
 		print ('primo')
 		break
 
+'''
