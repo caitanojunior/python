70e6f453a067805500d82a9666a639e46f9c381a
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-03-27 17:33:36 -0300

    execircio primalidade

--- Coursera/semana4/primalidade.py
+++ Coursera/semana4/primalidade.py
@@ -1,6 +1,5 @@
 numero = int(input('Digite um número inteiro: '))
 
-
 i = 2
 ehprimo = True
 
@@ -13,17 +12,3 @@ if(ehprimo):
 	print('primo')
 else:
 	print('não é primo')
-
-
-'''
-for i in range(2, numero+1):
-	if i != numero:
-		i = numero % i
-		if i == 0:
-			print ('não primo')
-			break
-	else:
-		print ('primo')
-		break
-
-'''
