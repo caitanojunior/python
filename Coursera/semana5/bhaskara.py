2dd2f3bf2a3f329f676a1fd8777f14cf14b1af8a
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-03-29 09:26:50 -0300

    exercicio bhaskara com funcoes

--- Coursera/semana5/bhaskara.py
+++ Coursera/semana5/bhaskara.py
@@ -1,26 +1,27 @@
 import math
-a = float(input('Digite o valor de A: '))
-b = float(input('Digite o valor de B: '))
-c = float(input('Digite o valor de C: '))
 
-# delta = b² - 4ac
-def calcDelta (a, b, c):
+def main():
+    a = float(input('Digite o valor de A: '))
+    b = float(input('Digite o valor de B: '))
+    c = float(input('Digite o valor de C: '))
+    imprime_raizes(a, b, c)
+
+# calcula --> delta = b² - 4ac
+def delta(a, b, c):
     return b**2 - 4*a*c
 
-def bhaskara():
-    return (-b + math.sqrt(calcDelta(a, b, c))) / (2 * a)
-
-
-if(calcDelta(a, b, c) == 0):
-    bhaskara()
-    print('a raiz desta equação é', bhaskara())
-else:
-    if(calcDelta(a, b, c) < 0):
-        print('esta equação não possui raízes reais')
+def imprime_raizes(a, b, c):
+    d = delta(a, b, c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d))/(2 * a)
+        print("A única raiz é:", raiz1)
     else:
-        bhaskara()
-        raiz2 = (-b - math.sqrt(calcDelta(a, b, c))) / (2 * a)
-        if(bhaskara() < raiz2):
-            print('as raízes da equação são',bhaskara(), 'e', raiz2)
+        if d < 0:
+            print("Esta equação não possui raízes reais")
         else:
-            print('as raízes da equação são', raiz2, 'e', bhaskara())
+            raiz1 = (-b + math.sqrt(d))/(2 * a)
+            raiz2 = (-b + math.sqrt(d))/(2 * a)
+            print("A primeira raiz é:", raiz1)
+            print("A segunda raiz é:", raiz2)
+
+main()
