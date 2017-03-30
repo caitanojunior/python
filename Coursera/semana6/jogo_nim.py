6780dddbaab08c5634ff9664c813ca01585e9300
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-03-30 14:25:10 -0300

    criação dos metodos partida, comp_escolhe_jogada e usuario_escolhe_jogada

--- Coursera/semana6/jogo_nim.py
+++ Coursera/semana6/jogo_nim.py
@@ -1,20 +1,48 @@
+import random
+
 def partida():
-    n = int(input("Digite o número de peças do jogo: "))
-    m = int(input("Digite o número máximo de peças a retirar: "))
+    n = int(input("Quantas peças? "))
+    m = int(input("Limite de peças por jogada? "))
     if n % (m + 1) == 0:
         usuario_escolhe_jogada(n, m)
     else:
         computador_escolhe_jogada(n, m)
 
 def computador_escolhe_jogada(n, m):
-    jogada = 1
-    n = n - (m + 1)
-    while jogada < m and retante % (m + 1) == 0:
-        return jogada
-        jogada = jogada + 1
-        print(jogada)
+    if(m > 1):
+        jogada = random.randint(1, m -1)
+    else:
+        jogada = 1
+
+    peçasRestante = n - jogada
+
+    if peçasRestante % (m + 1) == 0:
+        n = n - jogada
+        print("O computador tirou", jogada, "peça.")
+        print("Agora restam", n, "peças no tabuleiro.")
+        if (m <= n):
+            usuario_escolhe_jogada(n, m)
+        else:
+            print("Fim do jogo! O computador ganhou!")
+    else:
+        jogada = m
+        n = n - jogada
+        print("O computador tirou", jogada, "peça.")
+        print("Agora restam", n, "peças no tabuleiro.")
+        if(m <= n):
+            usuario_escolhe_jogada(n, m)
+        else:
+            print("Fim do jogo! O computador ganhou!")
 
 def usuario_escolhe_jogada(n, m):
+    jogada = int(input("Quantas peças você vai tirar? "))
+    n = n - jogada
+    print("Você tirou", jogada, "peça.")
+    print("Agora restam", n, "peças no tabuleiro.")
+    if (m <= n):
+        computador_escolhe_jogada(n, m)
+    else:
+        print("Fim do jogo! Você ganhou!")
 
 
 partida()
