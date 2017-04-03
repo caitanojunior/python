25d4b25a2b6ad886af5e5a2f006c9a79120129e8
Caitano <jose.caitano@virtus.ufcg.edu.br>
2017-04-03 17:39:27 -0300

    implementação da lógica campeonato

--- Coursera/semana6/jogo_nim.py
+++ Coursera/semana6/jogo_nim.py
@@ -1,7 +1,7 @@
 import random
 
-você = 0
 computador = 0
+você = 0
 
 def partida():
     n = int(input("Quantas peças? "))
@@ -11,61 +11,80 @@ def partida():
     else:
         computador_escolhe_jogada(n, m)
 
+def campeonato():
+    i = 1
+    global computador
+    global você
+    while i <= 3:
+        print("**** Rodada", i, "****")
+        partida()
+        i = i + 1
+        computador = computador + 1
+        você = você + 1
+    print("**** Final do campeonato! ****")
+    print("Placar: Você", você - 1, "X", computador - 1, "Computador")
+
 def computador_escolhe_jogada(n, m):
     if(m > 1):
         jogada = random.randint(1, m -1)
+        if(jogada > n):
+            jogada = random.randint(1, n)
+        else:
+            jogada = jogada
     else:
         jogada = 1
-
     peçasRestante = n - jogada
-
     if peçasRestante % (m + 1) == 0:
         n = n - jogada
         print("O computador tirou", jogada, "peça.")
         print("Agora restam", n, "peças no tabuleiro.")
-        if (m <= n):
-            usuario_escolhe_jogada(n, m)
-        else:
-            computador = 1
+        if(n < 0):
+            n = 0
+            print("Agora restam", n, "peças no tabuleiro.")
+        if (n == 0):
             print("Fim do jogo! O computador ganhou!")
+            global  computador
+            computador = 1
+        else:
+            usuario_escolhe_jogada(n, m)
     else:
         jogada = m
         n = n - jogada
         print("O computador tirou", jogada, "peça.")
-        print("Agora restam", n, "peças no tabuleiro.")
-        if(m <= n):
-            usuario_escolhe_jogada(n, m)
-        else:
-            computador = 1
+        if (n < 0):
+            n = 0
+            print("Agora restam", n, "peças no tabuleiro.")
+        if (n == 0):
             print("Fim do jogo! O computador ganhou!")
+            computador = 1
+        else:
+            usuario_escolhe_jogada(n, m)
+
 
 def usuario_escolhe_jogada(n, m):
     jogada = int(input("Quantas peças você vai tirar? "))
-    n = n - jogada
-    print("Você tirou", jogada, "peça.")
-    print("Agora restam", n, "peças no tabuleiro.")
-    if (m <= n):
-        computador_escolhe_jogada(n, m)
+    if(jogada > n):
+        print("Jogada inválida, escolha um valor maior ou igual a", n)
+        usuario_escolhe_jogada(n, m)
     else:
-        você = 1
-        print("Fim do jogo! Você ganhou!")
+        n = n - jogada
+        print("Você tirou", jogada, "peça.")
+        print("Agora restam", n, "peças no tabuleiro.")
+        if (n == 0):
+            global você
+            você = 1
+            print("Fim do jogo! Você ganhou!")
+        else:
+            computador_escolhe_jogada(n, m)
 
 print("Bem-vindo ao jogo do NIM! Escolha:")
 print("1 - para jogar uma partida isolada")
 print("2 - para jogar um campeonato")
 opção = int(input())
 
-if(opção == 1):
+if (opção == 1):
     partida()
-elif(opção == 2):
-    i = 0
-    while i < 3:
-        partida()
-        i = i + 1
-        você = você + 1#verificar lógica
-        computador = computador + 1#verificar lógica
-    print("**** Final do campeonato! ****")
-    print("Placar: Você", você, "X", computador, "Computador")
+elif (opção == 2):
+    campeonato()
 else:
-    print("Opção inválida,")
-
+    print("Opção inválida")
