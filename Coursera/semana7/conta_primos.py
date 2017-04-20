da47a52bb03ef145b0fbcce82cabaf73bdce7f92
caitanojunior <caitanojunior@gmail.com>
2017-04-20 09:46:06 -0300

    conta quantos primos tem entre 2 e um número qualquer

--- Coursera/semana7/conta_primos.py
+++ Coursera/semana7/conta_primos.py
@@ -1,14 +1,19 @@
 def n_primos(x):
-    i = 2
-    while i <= x:
-        while x % 2 == 0 and x != 2:
-            print(i)
-            i = i + 1
+    x = n
+    if x == 2 :
+        quant = 1
+    elif x == 3:
+        quant = 2
+    elif x <= 6:
+        quant = 3
+    else:
+        quant = 4
 
+    while x >= 2:
+        if x % 2 != 0 and x % 3 != 0 and x % 5 != 0 and x % 7 != 0:
+            quant = quant + 1
+        x = x - 1
+    return quant
 
 n = int(input("Digite um número inteiro maior ou igual a 2: "))
-
-n_primos(n)
-
-
-# corrigir código e terminar
+print(n_primos(n))
